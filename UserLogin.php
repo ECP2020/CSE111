c925488947685e7ca6797510b797063c3205fd00
ECP2020 <37675657+ECP2020@users.noreply.github.com>
2020-12-05 22:01:57 -0800

    Add files via upload

---
+++ UserLogin.php
@@ -0,0 +1,55 @@
+<?php 
+
+// Check connection
+$conn = new SQLite3("data.sqlite");
+// Check connection
+    if(!$conn) 
+    {
+        die( "Not connected to DB");
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset ="UTF-8">
+
+   
+    <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0"/>
+
+    <link href = "css/styles.css" rel = "stylesheet" type = "text/css" > 
+    <!-- TITLE --> 
+    <title>Trending Songs</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+</head>
+<header> <!-- adding the css styles -->
+<body bgcolor = #a45db8> 
+
+<div class="sidebar">
+  <a href = "Index.php"> Home </a>
+  <a href="TrendingSongs.php">Trending Songs</a>
+  <a href="Albums.php">Albums</a>
+  <a href="RadioStations.php">Radio Stations</a>
+  <a href="UserLogin.php">Login</a>
+  <a href= "PastYears.php"> Past Trending Songs </a>
+</div>
+<center><a class = "logo"  href = UserLogin.php ><img id="logo" src ="css/img/2.png"  width="200px" height="200px" /></a></center>
+
+<center> 
+
+<form action= Login.php method="POST">
+		<label for="emailField">Email Address: </label>
+		<input name="userEmail" placeholder="123@gmail.com" id="emailField">
+        <input type = "password" placeholder = "Password" required >
+		<button type="submit" class="btn btn-outline-warning" name="submit" value="troll">Log-In</button>	
+</form>
+<!--sign up button -->
+<a href signup.php> Signup </a>
+<form action = logout.php method = "POST">
+</center>
+
+</body> 
+</header>
+</html>
