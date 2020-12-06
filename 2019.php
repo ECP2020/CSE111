d18ab705969d1611d79e17611fd446991351ac15
ECP2020 <37675657+ECP2020@users.noreply.github.com>
2020-12-06 12:34:31 -0800

    Add files via upload

---
+++ 2019.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <meta charset ="UTF-8">
+
+   
+    <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0"/>
+
+    <link href = "css/styles.css" rel = "stylesheet" type = "text/css" > 
+    <!-- TITLE --> 
+    <title>Trending Songs</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+</head>
+<header> <!-- adding the css styles -->
+<body bgcolor = #a45db8> 
+</body>
--- Login.php
+++ Login.php
@@ -1,3 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset ="UTF-8">
+
+   
+    <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0"/>
+
+    <link href = "css/styles.css" rel = "stylesheet" type = "text/css" > 
+    <!-- TITLE --> 
+    <title>Trending Songs</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+</head>
+<header> <!-- adding the css styles -->
+<body bgcolor = #a45db8> 
+    <div class="sidebar">
+        <a href = "Index.php"> Home </a>
+        <a href="TrendingSongs.php">Trending Songs</a>
+        <a href="Albums.php">Albums</a>
+        <a href="RadioStations.php">Radio Stations</a>
+        <a href= "PastYears.php"> Past Trending Songs </a>
+        <a href ="Logout.php" > Logout </a>
+</div>
+
+<center><a class = "logo"  href = Index.php ><img id="logo" src ="css/img/2.png"  width="200px" height="200px" /></a></center>
+</body> 
+</header>
+</html>
+
 <?php 
 
 // Check connection
@@ -20,23 +52,20 @@ $query= "SELECT P_email, P_name FROM Person WHERE P_email = '{$_SESSION['user_em
 $statement = $conn->prepare($query);
 $result = $statement->execute();
 
+while($row = $result->fetchArray(SQLITE3_ASSOC))
+{
+    $email = $row['P_email'];
+    $name = $row['P_name'];
+    echo "<center><h3> Welcome, " .$name. "</h3></center>"; 
+
+   
+}
 if($_SESSION['user_email'] = $result->fetchArray(SQLITE3_ASSOC)) 
 {
-    echo "<h3> Success </h3> ";
+    echo "<center><h3> Success </h3></center> ";
 
-    //$name;
-    //$email;
     //$_SESSION['user_email']= $row['P_email'];
     //$name = $row['P_name'];
-
-    while($row = $result->fetchArray(SQLITE3_ASSOC))
-    {
-        $email = $row['P_email'];
-        $name = $row['P_name'];
-        echo "<h3> Welcome ," .$name. "</h3> "; 
-
-        
-    } 
 
 
 } 
@@ -48,38 +77,6 @@ else
 
 
 
-    while($row = $result->fetchArray(SQLITE3_ASSOC))
-    {
-        $email = $row['P_email'];
-        $name = $row['P_name'];
-        echo "<h3> Welcome," .$name. "</h3> "; 
-
-        
-    } 
-
 
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset ="UTF-8">
-
-   
-    <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0"/>
-
-    <link href = "css/styles.css" rel = "stylesheet" type = "text/css" > 
-    <!-- TITLE --> 
-    <title>Trending Songs</title>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
-</head>
-<header> <!-- adding the css styles -->
-<body bgcolor = #a45db8> 
-
-</body> 
-</header>
-</html>
