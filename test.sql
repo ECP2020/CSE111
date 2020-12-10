92552b4d15c908deb43ba6366591890418c881da
ECP2020 <37675657+ECP2020@users.noreply.github.com>
2020-12-10 12:58:06 -0800

    Spotify playlist added to Global table

--- test.sql
+++ test.sql
@@ -41,15 +41,14 @@ FROM USA_TS;
 SELECT *
 FROM SelectedItems;
 
-DROP TABLE SelectedItems;
+SELECT * FROM Albums;
 
 
 CREATE TABLE SelectedItems(
                     checked varchar(25) null
                 );
 
-
-
+ALTER DATABASE [test] SET READ_WRITE WITH NO_WAIT;
 ALTER TABLE USA_TS
 ADD USA_TS_Spotify varchar(255) not null;
 
@@ -260,35 +259,266 @@ SET USA_TS_Spotify ='https://open.spotify.com/track/45S5WTQEGOB1VHr1Q4FuPl'
 WHERE USA_TS_title = 'Golden';
 
 
+---ADD HERE
+ALTER TABLE Global_TS
+ADD TS_Spotify varchar(255) null;
+
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/35mvY5S1H3J2QZyna3TFe0'
+WHERE TS_title = 'positions';
+
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/3tjFYV6RSFtuktYl3ZtYcq'
+WHERE TS_title LIKE '%Mood%';
+
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/02kDW379Yfd5PzW5A6vuGt'
+WHERE TS_title = 'Lemonade (feat. Gunna, Don Toliver & NAV)';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/0t1kP63rueHleOhQkYSXFY'
+WHERE TS_title LIKE '%Dynamite%';
+
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/4g7ErSF4oImNnITtyiFowz'
+WHERE TS_title LIKE '%What%you%know%bout%love';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/4Oun2ylbjFKMPTiaSbbCih'
+WHERE TS_title LIKE '%WAP%';
+
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/4y4spB9m0Q6026KfkAvy9Q'
+WHERE TS_title = 'Lonely (with benny blanco)';
+
+--Hawaii
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/4uoR6qeWeuL4Qeu2qJzkuG'
+WHERE TS_title = 'Hawái';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b'
+WHERE TS_title LIKE '%Blinding%';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/5u1n1kITHCxxp8twBcZxWy'
+WHERE TS_title LIKE '%Holy%';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/6UelLqGlWMcVH1E5c4H7lY'
+WHERE TS_title LIKE '%Watermelon%';
+
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/0PvFJmanyNQMseIFrU708S'
+WHERE TS_title LIKE '%For%the%Night%';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/6cx06DFPPHchuUAcTxznu9'
+WHERE TS_title = 'Head & Heart (feat. MNEK)';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/45bE4HXI0AwGZXfZtMp8JR'
+WHERE TS_title LIKE '%you%broke%me%first%';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/6AGOKlMZWLCaEJGnaROtF9'
+WHERE TS_title = 'Put Your Records On';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/11EnQRgRMJwMAesfkB5pnu'
+WHERE TS_title = 'Se Te Nota (with Guaynaa)';
+
+--
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/7ytR5pFWmSjzHJIeQkgog4'
+WHERE TS_title LIKE '%ROCKSTAR%';
+
+--realacion
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/5KCbr5ndeby4y4ggthdiAb'
+WHERE TS_title = 'Relación - Remix';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/5KCbr5ndeby4y4ggthdiAb'
+WHERE TS_title LIKE '%Wonder%';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/4xqrdfXkTW4T0RauPLv3WA'
+WHERE TS_title LIKE '%Heather%';
+
+--savage love
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/1xQ6trAsedVPCdbtDAmk0c'
+WHERE TS_title = 'Savage Love (Laxed - Siren Beat)';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/7qEHsqek33rTcFNT9PFqLf'
+WHERE TS_title = 'Someone You Loved';
+
+--roses
+
+--la toxica
+
+--vida de rico
+
+--un dia 
+
+--take you danciing 
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/2Wo6QQD1KMDWeFkkjLqwx5'
+WHERE TS_title = 'Roses - Imanbek Remix';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/15OjBeqNX3bShsGYQbteRv'
+WHERE TS_title = 'La Tóxica';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/73nAK3HgQK8dak83Y2WQ8F'
+WHERE TS_title = 'Vida de Rico';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/0EhpEsp4L0oRGM0vmeaN5e'
+WHERE TS_title = 'UN DIA (ONE DAY) (Feat. Tainy)';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/59qrUpoplZxbIZxk6X0Bm3'
+WHERE TS_title = 'Take You Dancing';
 
 
 
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/0ofHAoxe9vBkTCp2UQIavz'
+WHERE TS_title LIKE '%Dreams%';
+
+--La curiosidad 
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/4HYDUMY0xSpeBr0AMY9cUz'
+WHERE TS_title = 'La Curiosidad';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/7ce20yLkzuXXLUhzIDoZih'
+WHERE TS_title = 'Before You Go';
+
+--se te nota
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/11EnQRgRMJwMAesfkB5pnu'
+WHERE TS_title = 'Before You Go';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/2J4P46vCFm1rPkNkp9pZWX'
+WHERE TS_title = 'Ice Cream (with Selena Gomez)';
+
+--ice cream by selena
+
+
+--breaking me 
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/3H7ihDc1dqLriiWXwsc2po'
+WHERE TS_title = 'Breaking Me';
+
+--midnight sky
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/27ycaQnQAxaPiyeg3nr2aB'
+WHERE TS_title = 'Midnight Sky';
+
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/6EDO9iiTtwNv6waLwa1UUq'
+WHERE TS_title LIKE '%POPSTAR%';
+
+--dance monkey
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/2XU0oxnq2qxCpomAAuJY8K'
+WHERE TS_title = 'Dance Monkey';
+
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/4Ws314Ylb27BVsvlZOy30C'
+WHERE TS_title = 'Lovesick Girls';
+--lovestick girls
+
+--una locura
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/5y7DaS3jAc9eZzcSOoUIwU'
+WHERE TS_title = 'Una Locura';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/463CkQjx2Zk1yXoBuierM9'
+WHERE TS_title = 'Levitating (feat. DaBaby)';
+
+--jeans
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/3GtnId3Cm1Xa6joCj08nzW'
+WHERE TS_title = 'Jeans';
+
+--ay dios mio 
+
+UPDATE Global_TS
+SET TS_Spotify = 'https://open.spotify.com/track/7x4ASXYEKfQBCewcZhK776'
+WHERE TS_title = 'Ay, DiOs Mío!';
+
+--despeinada
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/0JAZWvvqttGFm4UMDsPQXC'
+WHERE TS_title = 'Despeinada';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/7ndTONDDRFGiPnnhOzOXxq'
+WHERE TS_title = 'La Nota';
+
+--la nota 
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/2Y0wPrPQBrGhoLn14xRYCG'
+WHERE TS_title LIKE '%Come%Go%';
 
 
 
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/45S5WTQEGOB1VHr1Q4FuPl'
+WHERE TS_title = 'Golden';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/0AUvWawuP0ibk4SQ3sIZjk'
+WHERE TS_title = 'Daisy';
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/5rZlwNFl01HqLWBQGryKSm'
+WHERE TS_title = 'Mood Swings (feat. Lil Tjay)';
+
+--how you like that 
+
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/4SFknyjLcyTLJFPKD2m96o'
+WHERE TS_title = 'How You Like That';
 
 
 
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/6IBcOGPsniK3Pso1wHIhew'
+WHERE TS_title = 'Forever After All';
 
+--more 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+UPDATE Global_TS
+SET TS_Spotify ='https://open.spotify.com/track/6juLaduD4STCUDWT0AYun4'
+WHERE TS_title = 'MORE';
 
 
 
